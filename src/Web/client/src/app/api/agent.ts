c23bdaaf4ad0a191518ab4328de9be5fa0f904c3
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-01 08:15:39 +0200

    Add frontend

---
+++ src/Web/client/src/app/api/agent.ts
@@ -0,0 +1,98 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
+import { history } from "../..";
+import { PaginatedResponse } from "../models/pagination";
+import { store } from "../store/configureStore";
+
+const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
+
+axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.withCredentials = true;
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+axios.interceptors.request.use((config: any) => {
+    const token = store.getState().account.user?.token;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
+
+axios.interceptors.response.use(async response => {
+    await sleep();
+    const pagination = response.headers['pagination'];
+    if (pagination) {
+        response.data = new PaginatedResponse(response.data, JSON.parse(pagination));
+        return response;
+    }
+    return response;
+}, (error: AxiosError<any, undefined>) => {
+    const { data, status } = error.response!;
+    switch (status) {
+        case 400:
+            if (data.errors) {
+                const modelStateErrors: string[] = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modelStateErrors.push(data.errors[key]);
+                    }
+                }
+                throw modelStateErrors.flat();
+            }
+            toast.error(data.title);
+            break;
+        case 401:
+            toast.error(data.title);
+            break;
+        case 500:
+            history.push({
+                pathname: '/server-error',
+                state: { error: data }
+            });
+            break;
+        default:
+            break;
+    }
+    return Promise.reject(error.response);
+})
+
+const requests = {
+    get: (url: string, params?: URLSearchParams) => axios.get(url, { params }).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    delete: (url: string) => axios.delete(url).then(responseBody)
+}
+
+const Catalog = {
+    list: (params: URLSearchParams) => requests.get('products', params),
+    details: (id: number) => requests.get(`products/${id}`),
+    fetchFilters: () => requests.get('products/filters')
+}
+
+const TestErrors = {
+    get400Error: () => requests.get('errors/bad-request'),
+    get401Error: () => requests.get('errors/unauthorised'),
+    get404Error: () => requests.get('errors/not-found'),
+    get500Error: () => requests.get('errors/server-error'),
+    getValidationError: () => requests.get('errors/validation-error'),
+}
+
+const Basket = {
+    get: () => requests.get('baskets'),
+    addItem: (productId: number, quantity = 1) => requests.post(`baskets?productId=${productId}&quantity=${quantity}`, {}),
+    removeItem: (productId: number, quantity = 1) => requests.delete(`baskets?productId=${productId}&quantity=${quantity}`)
+}
+
+const Account = {
+    login: (values: any) => requests.post('accounts/login', values),
+    register: (values: any) => requests.post('accounts/register', values),
+    currentUser: () => requests.get('accounts/currentUser')
+}
+
+const agent = {
+    Catalog,
+    TestErrors,
+    Basket,
+    Account
+}
+
+export default agent;
---
+++ src/Web/client/src/app/layout/App.tsx
@@ -0,0 +1,85 @@
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import AboutPage from "../../features/about/AboutPage";
+import Catalog from "../../features/catalog/Catalog";
+import ProductDetail from "../../features/catalog/ProductDetail";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import Header from "./Header";
+import "react-toastify/dist/ReactToastify.css";
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
+import BasketPage from "../../features/basket/BasketPage";
+import LoadingComponent from "./LoadingComponent";
+import CheckoutPage from "../../features/checkout/CheckoutPage";
+import { useAppDispatch } from "../store/configureStore";
+import { fetchBasketAsync } from "../../features/basket/basketSlice";
+import Login from "../../features/account/Login";
+import Register from "../../features/account/Register";
+import { fetchCurrentUser } from "../../features/account/accountSlice";
+
+function App() {
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  const initApp = useCallback(async () => {
+    try {
+      await dispatch(fetchCurrentUser());
+      await dispatch(fetchBasketAsync());
+    } catch (error) {
+      console.log(error);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    initApp().then(() => setLoading(false));
+  }, [initApp]);
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  if (loading) return <LoadingComponent message="Initialising app..." />;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/catalog" component={Catalog} />
+          <Route path="/catalog/:id" component={ProductDetail} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route path="/server-error" component={ServerError} />
+          <Route path="/basket" component={BasketPage} />
+          <Route path="/checkout" component={CheckoutPage} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route component={NotFound} />
+        </Switch>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
